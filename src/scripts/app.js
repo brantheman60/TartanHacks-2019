3b3df5ebf3b789ed26ecb0f842176545aa4006a0
Yuchen Shi <39347410+yus71@users.noreply.github.com>
2019-02-09 11:41:49 -0500

    Add files via upload

---
+++ src/scripts/app.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+
+  var app = {
+    isLoading: true,
+    visibleCards: {},
+    selectedCities: [],
+    spinner: document.querySelector('.loader'),
+    cardTemplate: document.querySelector('.cardTemplate'),
+    container: document.querySelector('.main'),
+    addDialog: document.querySelector('.dialog-container'),
+  };
+
+
+
+
+
+
+  // TODO add service worker code here
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
